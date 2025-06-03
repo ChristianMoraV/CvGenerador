f96d5e867c4579b9a47356a8a12f0b72738c6abe
lavaquita91 <christianmoises15@hotmail.com>
2025-06-03 18:46:41 -0500

    Script para deplyar en gh-pages

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/CvGenerador',
+  basePath: '/',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/assets-chunks/index_csr_html.mjs
+++ docs/server/assets-chunks/index_csr_html.mjs
@@ -3,7 +3,7 @@ export default `<!doctype html>
 <head>
   <meta charset="utf-8">
   <title>CvApp</title>
-  <base href="/CvGenerador/">
+  <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 <style>*{box-sizing:border-box;margin:0;padding:0}body{font-family:Inter,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol;color:#333;line-height:1.6}:root{--bright-blue:oklch(51.01% .274 263.83);--electric-violet:oklch(53.18% .28 296.97);--french-violet:oklch(47.66% .246 305.88);--vivid-pink:oklch(69.02% .277 332.77);--hot-red:oklch(61.42% .238 15.34);--orange-red:oklch(63.32% .24 31.68);--gray-900:oklch(19.37% .006 300.98);--gray-700:oklch(36.98% .014 302.71);--gray-400:oklch(70.9% .015 304.04)}@media print{@page{size:A4;margin:0}body{width:100%;height:100%;margin:0;padding:0;background-color:#fff}}@page{size:A4;margin:0}</style><link rel="stylesheet" href="styles-6VZPZ37R.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-6VZPZ37R.css"></noscript></head>
