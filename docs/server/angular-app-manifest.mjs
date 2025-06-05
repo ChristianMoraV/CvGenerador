a8c2e11b234f59db5cef69a365ad81754c9de681
ChristianMoraV <christianmoises15@hotmail.com>
2025-06-05 08:49:24 -0500

    Build

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1381, hash: '1c6cd24257fc6401164b62a99c63f7a255644474139cbb72e609330e288103ea', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1065, hash: '78e37a59be83810a25e3f66862a0e8120f0b713a3089fbdba5e9576a1ec98372', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 44311, hash: 'ff70d38f394857f21a1b35f5cc7906544976cac73184271144cd5482c9e45c80', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1381, hash: '96df43f48e672c7d14056f748da06c1393faeb518ec9cd71b1ef647a6fdd32fe', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1065, hash: '69b9e0f368e5f8c07cc8b08eb0a2c6a864c8ca06d5f40aa9ec4bda141949b855', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 44311, hash: '16b89d511c22165f68a8037dbc747090266181e6da33857c357d920eda3fed63', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-6VZPZ37R.css': {size: 977, hash: 'm0wQMwTgNhs', text: () => import('./assets-chunks/styles-6VZPZ37R_css.mjs').then(m => m.default)}
   },
 };
--- docs/server/assets-chunks/index_csr_html.mjs
+++ docs/server/assets-chunks/index_csr_html.mjs
@@ -9,6 +9,6 @@ export default `<!doctype html>
 <style>*{box-sizing:border-box;margin:0;padding:0}body{font-family:Inter,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol;color:#333;line-height:1.6}:root{--bright-blue:oklch(51.01% .274 263.83);--electric-violet:oklch(53.18% .28 296.97);--french-violet:oklch(47.66% .246 305.88);--vivid-pink:oklch(69.02% .277 332.77);--hot-red:oklch(61.42% .238 15.34);--orange-red:oklch(63.32% .24 31.68);--gray-900:oklch(19.37% .006 300.98);--gray-700:oklch(36.98% .014 302.71);--gray-400:oklch(70.9% .015 304.04)}@media print{@page{size:A4;margin:0}body{width:100%;height:100%;margin:0;padding:0;background-color:#fff}}@page{size:A4;margin:0}</style><link rel="stylesheet" href="styles-6VZPZ37R.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-6VZPZ37R.css"></noscript></head>
 <body ngcm="">
   <app-root></app-root>
-<link rel="modulepreload" href="chunk-EQDQRRRY.js"><script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-QRA26LLF.js" type="module"></script></body>
+<link rel="modulepreload" href="chunk-EQDQRRRY.js"><script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-AUJKJ3XK.js" type="module"></script></body>
 </html>
 `;
